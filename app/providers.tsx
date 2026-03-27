604f5cbbabcbe481e6bede8b434417454465b057
nippysky <nsinfo247@gmail.com>
2026-03-27 08:34:03 +0100

    revamped wallet provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,8 @@ import * as React from "react";
 import { ThemeProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThirdwebProvider } from "thirdweb/react";
+import { ConnectedWalletProvider } from "@/src/providers/ConnectedWalletProvider";
+import { UnifiedWalletProvider } from "@/src/providers/UnifiedWalletProvider";
 
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -13,7 +15,11 @@ export function Providers({ children }: { children: React.ReactNode }) {
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <QueryClientProvider client={queryClient}>
         <ThirdwebProvider>
-          {children}
+          <ConnectedWalletProvider>
+            <UnifiedWalletProvider>
+              {children}
+            </UnifiedWalletProvider>
+          </ConnectedWalletProvider>
         </ThirdwebProvider>
       </QueryClientProvider>
     </ThemeProvider>
